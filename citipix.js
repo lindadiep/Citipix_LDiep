f2cbbc9e40554cef04032e9ba3b2aee2a97a858d
Joni Halabi <joni@jhalabi.com>
2016-01-30 23:53:32 -0500

    Feedback on homework #5

--- citipix.js
+++ citipix.js
@@ -1,9 +1,19 @@
-
+/*
+ * Hi Linda!  I am adding feedback about your code in Javascript comments like this on.
+ * 
+ * Overall, great job!  The page functions just like it should.  
+ * One comment about code style - make sure you watch how you indent your code and be sure
+ * to indent consistently.
+ * 
+ * Fantastic use of comments throughout the code.  More feedback below!
+ */
 
 
 $(document).ready(function() {
 
 	// user clicks button
+
+    // Very nice use of an anonymous function here!  :)
 	$('form').submit(function(event){
 		event.preventDefault();
 
@@ -11,9 +21,25 @@ $(document).ready(function() {
     var city = $('#city-type').val();
     //console.log(city);
 
+    // ^^ Nice use of console.log() - I'm assuming you were using that to debug?
+
 		// background changes
 
+        // The if-else statement looks fantastic!
 		if (city == 'NYC' || city == 'New York City' || city == 'New York') {
+
+            /* Using the attr() jQuery function is definitely a great way to change the
+             * class here.  As an alternative, you could also use the addClass() function
+             * to add the appropriate class and the removeClass() function to remove any other
+             * classes that may have been on the body element before, like this:
+             *
+             * $('body').removeClass().addClass('nyc');
+             *
+             * Nothing wrong with using attr() to do this; it is probably more efficient, in fact,
+             * since you are only using 1 function instead of 2.  Just wanted to make sure
+             * you know about the alternative.  :)
+             */
+
 			$('body').attr('class','nyc');
     	} 
 
